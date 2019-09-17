11af01a18dc46bd1b71c7f6468fe355ada0bad3b
Fredrick Lambuth <flambuth@gmail.com>
2019-09-17 16:51:14 -0500

    doing more sqlite stuff

--- sqlite/sqlite_tutorial.py
+++ sqlite/sqlite_tutorial.py
@@ -34,3 +34,40 @@ def create_table(conn, create_table_sql):
  
 # if __name__ == '__main__':
 #     create_connection(r"/Users/fredricklambuth/Documents/Notes/PYTHON/sqlite/first_database.db")
+def main():
+    database = r"/Users/fredricklambuth/Documents/Notes/PYTHON/sqlite/first_database.db"
+ 
+    sql_create_projects_table = """ CREATE TABLE IF NOT EXISTS projects (
+                                        id integer PRIMARY KEY,
+                                        name text NOT NULL,
+                                        begin_date text,
+                                        end_date text
+                                    ); """
+ 
+    sql_create_tasks_table = """CREATE TABLE IF NOT EXISTS tasks (
+                                    id integer PRIMARY KEY,
+                                    name text NOT NULL,
+                                    priority integer,
+                                    status_id integer NOT NULL,
+                                    project_id integer NOT NULL,
+                                    begin_date text NOT NULL,
+                                    end_date text NOT NULL,
+                                    FOREIGN KEY (project_id) REFERENCES projects (id)
+                                );"""
+ 
+    # create a database connection
+    conn = create_connection(database)
+ 
+    # create tables
+    if conn is not None:
+        # create projects table
+        create_table(conn, sql_create_projects_table)
+ 
+        # create tasks table
+        create_table(conn, sql_create_tasks_table)
+    else:
+        print("Error! cannot create the database connection.")
+ 
+ 
+if __name__ == '__main__':
+    main()
