40c23393de2edc47055d7410b0810f044c1d9a12
Fredrick Lambuth <flambuth@gmail.com>
2019-09-18 15:58:32 -0500

    sqlite cursor executes are finicky

--- checkbook_deluxe/checkbook_Deluxe.py
+++ checkbook_deluxe/checkbook_Deluxe.py
@@ -1,128 +1,37 @@
 #!/usr/local/anaconda3/bin/python
 
 
-import json
-json_file = 'customers.json'
-
-with open(json_file, "r") as f: 
-    customers = json.load(f)
-
-
-def check_user(user):
-    user_names = [i['name'] for i in customers]
-    return user in user_names
-
-def create_user(user):
-    balance = int(input("How much do you have to start your account? "))
-    new_user = {
-        'name': user,
-        'balance' : balance
-    }
-    customers.append(new_user)
-
-# scaffolding for using sqlite
-# def insert_customer(emp):
-#     with conn:
-#         c.execute("INSERT INTO customers VALUES (:name, :balance)", {'name': emp.first, 'balance': emp.last})
-
-
-def see_balance(user, user_list):
-    user_balance = [i['balance'] for i in user_list if i['name'] == user]
-    return user_balance
-
-def add_to_balance(user, amount):
-    for i in customers: 
-        if i['name'] == user: 
-            i['balance'] += amount 
-#     I'm gonna have to make this persistent. Maybe add a transactions table structure.
-#     transaction_list.append(amount)
-
-#Will only let you go negative once. Make a big withdrawal and run for it.
-def subtract_from_balance(user, amount):
-    for i in customers: 
-        if i['name'] == user:
-            if i['balance'] < 1:
-                print("Your balance is not large enough to support a withdrawal.") 
-            else:
-                i['balance'] -= amount
-
-#SKUNKWORKS: lets make a transaction history
-#def show_transactions():
-
-
-
-def print_menu():
-    print("\n")
+def print_menu():       ## Your menu design here
     print( 30 * "-" , "MENU" , 30 * "-")
-    print( "1. View Current Balance")
-    print( "2. Make A Deposit")
-    print( "3. Make A Withdrawal")
-    print( "4. Exit")
+    print( "1. Menu Option 1")
+    print( "2. Menu Option 2")
+    print( "3. Menu Option 3")
+    print( "4. Menu Option 4")
+    print( "5. Exit")
     print( 67 * "-")
-
-
-##############################
-transaction_list = []
-
-
+  
 loop=True      
-
-current_user = input("What is your user name? ")
-if check_user(current_user):
-    print(f"Welcome back {current_user}")
-else:
-    print(" A new user!")
-    create_user(current_user)
-
-# for i in customers:
-#     if i['balance'] < 1 and i['name'] == current_user:
-#         print ("Please deposit more money before using the withdrawal function. ")
-        
-
-
+  
 while loop:          ## While loop which will keep going until loop = False
-    #test to see if account is in arrears 
-    for i in customers:
-        if i['balance'] < 1 and i['name'] == current_user:
-            print ("Please deposit more money before using the withdrawal function. ".upper())
-            break
-    
     print_menu()    ## Displays menu
-    choice = int(input("Enter your choice [1-4]: "))
+    choice = int(input("Enter your choice [1-5]: "))
      
     if choice==1:     
-        print("Menu 1 has been selected") 
-        current_balance = see_balance(current_user, customers)
-        print(f"Your current balance is ${str(current_balance)[1:-1]}")
-    
+        print( "Menu 1 has been selected")
+        ## You can add your code or functions here
     elif choice==2:
-        print("Menu 2 has been selected")
-        amount = int(input("How much are you depositing to your account? "))
-        add_to_balance(current_user, amount)
-        current_balance = see_balance(current_user, customers)
-        print(f"Your current balance is ${str(current_balance)[1:-1]}")
-    
+        print( "Menu 2 has been selected")
+        ## You can add your code or functions here
     elif choice==3:
-        print("Menu 3 has been selected")
-        amount = int(input("How much are you withdrawing from your account? "))
-        subtract_from_balance(current_user, amount)
-        current_balance = see_balance(current_user, customers)
-        print(f"Your current balance is ${str(current_balance)[1:-1]}")
-    
-    # elif choice==4:
-    #     print("Menu 4 has been selected")
-    #     print("More features to come!")
-        ## The exit choice to break the while loop
+        print( "Menu 3 has been selected")
+        ## You can add your code or functions here
     elif choice==4:
-        print("Menu 4 has been selected")
-        print("Good bye")
-        break
-
+        print( "Menu 4 has been selected")
+        ## You can add your code or functions here
+    elif choice==5:
+        print( "Menu 5 has been selected")
+        ## You can add your code or functions here
         loop=False # This will make the while loop to end as not value of loop is set to False
     else:
         # Any integer inputs other than values 1-5 we print an error message
         print("Wrong option selection. Enter any key to try again..")
-
-#save the changes
-with open('customers.json', 'w') as f:  
-    json.dump(customers, f)
--- checkbook_deluxe/json2db.py
+++ checkbook_deluxe/json2db.py
@@ -3,9 +3,10 @@
 #Customers table has been transfereed. Working on making a transactions table.
 import json, sqlite3
 
-json_file = 'customers.json' 
+# json_file = "customers.json"
 
-with open(json_file) as f: 
+#customers list is a list of dictionaries
+with open('/Users/fredricklambuth/Documents/Notes/PYTHON/checkbook_deluxe/customers.json', 'r') as f: 
     customers_list = json.load(f)
 
 conn = sqlite3.connect('customers.db')
@@ -13,25 +14,31 @@ conn = sqlite3.connect('customers.db')
 c = conn.cursor()
 
 # Makes a blank table for cusomter name and balance
-# c.execute("""CREATE TABLE timestamps ( trans_id integer PRIMARY KEY, stamp text NOT NULL)
-# """)                                                                                                                    
+# c.execute("""CREATE TABLE transaction ( 
+#     id integer PRIMARY KEY, 
+#     user text NOT NULL,
+#     amount integer)""")    
 
+#I don't know why this works and the other two dont.
+c.execute("""CREATE TABLE customers (
+            username text PRIMARY KEY,
+            balance integer
+            )""")                                                                                                                  
 # Makes a transaction table. Finish up later
 # c.execute("""CREATE TABLE timestamps ( 
 #     name text PRIMARY KEY, 
-#     balance integer NOT NULL)
-#     """)
+#     balance integer NOT NULL)""")
 
 
 #The JSON was dumped into customer_list, which I iterated through and inserted each as a row
 #in the customers table the customers database
-# for i in customers_list: 
-#     c.execute("INSERT INTO customers VALUES (?,?)",(i['name'],i['balance'])) 
+for i in customers_list: 
+    c.execute("INSERT INTO customers VALUES (?,?)",(i['name'],i['balance'])) 
 
 #Verify the rows are in the table
 c.execute("SELECT * FROM customers")
 print(c.fetchall())   
 
 #Uncomment if you wanna make some changes. This is for documentation.
-#conn.comitt()
+conn.commit()
 conn.close()
